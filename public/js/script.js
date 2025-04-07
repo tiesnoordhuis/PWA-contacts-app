f060c848a737ccd7e580a36e24e4f773606c1094
Ties Noordhuis <ties@bit-academy.nl>
2025-04-07 11:31:49 +0200

    add storing profile with picture in mongodb

--- public/js/script.js
+++ public/js/script.js
@@ -19,3 +19,47 @@ stopButton.addEventListener('click', () => {
     camera.stopStream();
     videoElement.srcObject = null;
 });
+
+const profileTemplate = document.getElementById('profileTemplate');
+const profileForm = document.getElementById('profileForm');
+const takePhotoButton = document.getElementById('takePhoto');
+
+takePhotoButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const canvas = document.getElementById('photoCanvas')
+    const context = canvas.getContext('2d');
+    context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+});
+
+profileForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    // get the image data from the video element
+    const canvas = document.getElementById('photoCanvas')
+    // use high compression
+    const imageData = canvas.toDataURL('image/jpeg', 0.5);
+    
+    await fetch('/api/profile', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: formData.get('name'),
+            email: formData.get('email'),
+            photo: imageData
+        })
+    });
+})
+
+const profiles = await fetch('/api/profile')
+    .then(response => response.json())
+
+profiles.forEach(profile => {
+    const clone = profileTemplate.content.cloneNode(true);
+    clone.querySelector('.name').textContent = profile.name;
+    clone.querySelector('.email').textContent = profile.email;
+    clone.querySelector('img.photo').src = profile.photo;
+    document.getElementById('profilesContainer').appendChild(clone);
+})
--- server/server.js
+++ server/server.js
@@ -1,13 +1,31 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoClient } from 'mongodb';
 
 const app = express();
 const PORT = process.env.PORT ?? 6789;
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.fxtst7f.mongodb.net/?appName=Cluster0`;
 
 app.use(cors());
 app.use(express.json());
 
 app.use(express.static('../public'));
+
+const client = new MongoClient(uri);
+await client.connect()
+
+app.get('/api/profile', async (req, res) => {
+    const collection = client.db('application_database').collection('profile');
+    const profiles = await collection.find({}).toArray();
+    res.json(profiles);
+});
+
+app.post('/api/profile', async (req, res) => {
+    const collection = client.db('application_database').collection('profile');
+    const newProfile = req.body;
+    await collection.insertOne(newProfile);
+    res.status(201).json(newProfile);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
