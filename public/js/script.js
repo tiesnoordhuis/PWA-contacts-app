22438f45d2f31769fdeaef04b3f192358bb36d39
Ties Noordhuis <ties@bit-academy.nl>
2025-05-07 09:27:52 +0200

    simplfy code

--- public/js/script.js
+++ public/js/script.js
@@ -4,15 +4,10 @@ const camera = new Camera();
 const videoElement = document.getElementById('cameraFeed');
 const startButton = document.getElementById('startCamera');
 const stopButton = document.getElementById('stopCamera');
-const errorMessage = document.getElementById('errorMessage');
 
 startButton.addEventListener('click', async () => {
-    try {
-        videoElement.srcObject = await camera.getStream();
-        videoElement.play();
-    } catch (error) {
-        errorMessage.textContent = error.message;
-    }
+    videoElement.srcObject = await camera.getStream();
+    videoElement.play();
 });
 
 stopButton.addEventListener('click', () => {
